d87e46f1db5cc60785de9efe9143bf0d1d339f80
DKATyler <DKATyler@c345011a-f2d2-11dd-b507-8b4ecd5210be>
2009-02-18 05:32:50 +0000

    Updated whereClass to use drewOps, tableOps.

--- helperFunctions.java
+++ helperFunctions.java
@@ -3,21 +3,42 @@ import javax.swing.JTable;
 class helperFunctions {
 	//Converts a given variable into a JTable
 	public static JTable convertToTable(JTable table, String variable) {
+		variable=variable.trim();
+		//System.out.println("Parsing: " + variable);
+		drewOps drewOps=new drewOps();
 		JTable retTable;
 		int curCol=0;
 		for(curCol=0; curCol<table.getColumnCount() && table.getColumnName(curCol)!=variable; ++curCol);
-		if(curCol!=table.getColumnCount()) {
+//		//System.out.println(table.getColumnName(0));
+//		if(curCol!=table.getColumnCount()) {
+		if(variable.equals("A")) {
+		//System.out.println("Parsing3: " + variable);
+			//System.out.println("Converted to table");
 		    retTable=new JTable(table.getRowCount(), 1);
 			for(int curRow=0; curRow<table.getRowCount(); ++curRow)
-				retTable.setValueAt(table.getValueAt(curRow,curCol), curRow, 0);
+				retTable.setValueAt(table.getValueAt(curRow,0), curRow, 0);
+			//System.out.println("Converted to table success");
 		}
 		else {
-			retTable=createTable(table.getRowCount(), variable);
+			//System.out.println("Parsing4: " + variable);
+			if(variable.contains(".")) {
+				//System.out.println("Double: " + variable);
+				retTable=createTable(table.getRowCount(), Double.parseDouble(variable));
+			}
+			else if(variable==String.valueOf(drewOps.TRUE))
+				retTable=createTable(table.getRowCount(), drewOps.TRUE);				
+			else if(variable==String.valueOf(drewOps.FALSE))
+				retTable=createTable(table.getRowCount(), drewOps.FALSE);				
+			else try {
+				retTable=createTable(table.getRowCount(), Integer.parseInt(variable));
+			} catch(NumberFormatException e) {
+				retTable=createTable(table.getRowCount(), variable);
+			}
 		}
 		return retTable;
 	}
 	//creates a JTable with initial value variable, cols=1, and the given number of rows
-	public static JTable createTable(int rows, String variable) {
+	public static JTable createTable(int rows, Object variable) {
 	    JTable retTable=new JTable(rows, 1);
 		for(int curRow=0; curRow<rows; ++curRow)
 			retTable.setValueAt(variable, curRow, 0);
--- whereClass.java
+++ whereClass.java
@@ -10,6 +10,8 @@ class whereClass {
 	JTable table;
 
 	public static JTable where(JTable argTable, String commands) {
+		drewOps drewOps=new drewOps();
+		//System.out.println("Where Entry " + commands);
 		whereClass whereObj=new whereClass(argTable);
 		int selectIndex;
 		int subQueryIndex;
@@ -56,27 +58,33 @@ class whereClass {
 		Stack<Integer> openParens=new Stack<Integer>();
 		int oParen;
 		int cParen;
+		//System.out.println("Where Entry2 " + commands);
 		if((oParen=commands.indexOf('('))!=-1) {
+			//System.out.println("oParens" + commands);
 			openParens.push(oParen);
 			while(openParens.size()!=0) {
-				if(commands.indexOf('(', oParen+1) < commands.indexOf(')', oParen+1)) {
+				//System.out.println("oParens2" + commands);
+				if(commands.indexOf('(', oParen+1)!=-1 && (commands.indexOf('(', oParen+1) < commands.indexOf(')', oParen+1))) {
+					//System.out.println("oParens3" + "Result: " + commands.indexOf('(', oParen+1) + commands);
 					oParen=commands.indexOf('(', oParen);
 					openParens.push(oParen);
 				}
 				else {
 					oParen=openParens.pop();
 					cParen=commands.indexOf(')', oParen+1);
-					whereObj.subQueries.add(whereObj.runOperator(commands.substring(oParen+1, cParen-1)));
+					//System.out.println("oParens4" + commands.substring(oParen+1, cParen));
+					whereObj.subQueries.add(whereObj.runOperator(commands.substring(oParen+1, cParen)));
 					commands=commands.substring(0,oParen) +
 					whereObj.ctrlString + whereObj.subQueries.size() +
 					commands.substring(cParen+1, commands.length());
 				}
 			}
 		}
+		//System.out.println("Rest " + commands);
 		DefaultTableModel model =(DefaultTableModel)whereObj.table.getModel();
 		JTable results=whereObj.runOperator(commands);
 		for(int curRow=results.getRowCount()-1; curRow>0; --curRow) {
-			if((String)results.getValueAt(curRow, 0)=="FALSE")
+			if((String)results.getValueAt(curRow, 0)==drewOps.FALSE)
 				model.removeRow(curRow);
 		}
 		return whereObj.table;
@@ -88,130 +96,139 @@ class whereClass {
 	}
 	//Runs the given command
 	private JTable runOperator(String command) {
+		drewOps drewOps=new drewOps();
+		//System.out.println("Run " + command);
 		String[] args;
 		JTable lArg;
 		JTable rArg;
 		if(command.contains("AND")) {
 			args=command.split(" AND ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				//System.out.println(args[0].indexOf("ctrlString")+ctrlString.length()+1);
+				//System.out.println(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1))));
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			andOperator(rArg, lArg);
+			for(int ctr=0; ctr<lArg.getRowCount(); ++ctr) {
+				//System.out.println("AND" + lArg.getValueAt(ctr, 0));
+			}
+			return(drewOps.and(rArg, lArg));
 		}
 		else if(command.contains("OR")) {
 			args=command.split(" OR ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1))));
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			orOperator(rArg, lArg);
+			return drewOps.or(rArg, lArg);
 		}
 		else if(command.startsWith("NOT")) {
 			args=command.split(" NOT ");
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1))));
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			notOperator(lArg);
+			return drewOps.not(rArg);
 		}
 		else if(command.contains("!=")) {
 			args=command.split(" != ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1))));
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			!=Operator(rArg, lArg);
+			return drewOps.notEqual(rArg, lArg);
 		}
 		else if(command.contains("<=")) {
 			args=command.split(" <= ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1))));
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			<=Operator(rArg, lArg);
+			return drewOps.lessEqual(rArg, lArg);
 		}
 		else if(command.contains(">=")) {
 			args=command.split(" >= ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1))));
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			>=Operator(rArg, lArg);
+			return drewOps.greaterEqual(rArg, lArg);
 		}
 		else if(command.contains(">")) {
+			//System.out.println("Run2 " + command);
 			args=command.split(" > ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1))));
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			>Operator(rArg, lArg);
+			return drewOps.greaterThan(rArg, lArg);
 		}
 		else if(command.contains("<")) {
 			args=command.split(" < ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			<Operator(rArg, lArg);
+			return drewOps.lessThan(rArg, lArg);
 		}
 		else if(command.contains("=")) {
 			args=command.split(" = ");
 			if(args[0].startsWith(ctrlString)) {
-				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()))));
+				lArg=subQueries.elementAt(Integer.valueOf((args[0].substring(args[0].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				lArg=helperFunctions.convertToTable(table, args[0].trim());
 			if(args[1].startsWith(ctrlString)) {
-				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()))));
+				rArg=subQueries.elementAt(Integer.valueOf((args[1].substring(args[1].indexOf("ctrlString")+ctrlString.length()+1)))-1);
 			}
 			else
 				rArg=helperFunctions.convertToTable(table, args[1].trim());
-//			=Operator(rArg, lArg);
+			return drewOps.equals(rArg, lArg);
 		}
+		//Should be unreachable
 		return table;
 	}
 	//gets (...).....
