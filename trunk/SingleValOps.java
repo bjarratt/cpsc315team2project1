56665d11db4409ddcb397c4b4472daf697b8d882
DKATyler <DKATyler@c345011a-f2d2-11dd-b507-8b4ecd5210be>
2009-02-21 19:03:13 +0000

    implemented min, max, count, sum

--- trunk/SingleValOps.java
+++ trunk/SingleValOps.java
@@ -1,8 +1,61 @@
 class SingleValOps {
 	static Double max(Table myTable) {
-		return 0.0;
+		double maxVal=0;
+		if(myTable.getRowCount()>0) {
+			if(myTable.colType(0)==CompareOps.DOUBLE) {
+				maxVal=(Double) myTable.getValueAt(0,0);
+				for(int i=1; i<myTable.getRowCount(); ++i) {
+					if((Double) myTable.getValueAt(i,0)>maxVal)
+						maxVal=(Double) myTable.getValueAt(i,0);
+				}
+			}
+			else if(myTable.colType(0)==CompareOps.INTEGER) {
+				maxVal=(Integer) myTable.getValueAt(0,0);
+				for(int i=1; i<myTable.getRowCount(); ++i) {
+					if((Integer) myTable.getValueAt(i,0)>maxVal)
+						maxVal=(Integer) myTable.getValueAt(i,0);
+				}
+			}
+		}
+		return maxVal;
 	}
 	static Double min(Table myTable) {
-		return 0.0;
+		double minVal=0;
+		if(myTable.getRowCount()>0) {
+			if(myTable.colType(0)==CompareOps.DOUBLE) {
+				minVal=(Double) myTable.getValueAt(0,0);
+				for(int i=1; i<myTable.getRowCount(); ++i) {
+					if((Double) myTable.getValueAt(i,0)<minVal)
+						minVal=(Double) myTable.getValueAt(i,0);
+				}
+			}
+			else if(myTable.colType(0)==CompareOps.INTEGER) {
+				minVal=(Integer) myTable.getValueAt(0,0);
+				for(int i=1; i<myTable.getRowCount(); ++i) {
+					if((Integer) myTable.getValueAt(i,0)<minVal)
+						minVal=(Integer) myTable.getValueAt(i,0);
+				}
+			}
+		}
+		return minVal;
+	}
+	static int count(Table myTable) {
+		return(myTable.getRowCount());
+	}
+	static double sum(Table myTable) {
+		double sumVal=0;
+		if(myTable.colType(0)==CompareOps.DOUBLE) {
+			sumVal=(Double) myTable.getValueAt(0,0);
+			for(int i=1; i<myTable.getRowCount(); ++i) {
+				sumVal+=(Double) myTable.getValueAt(i,0);
+			}
+		}
+		else if(myTable.colType(0)==CompareOps.INTEGER) {
+			sumVal=(Integer) myTable.getValueAt(0,0);
+			for(int i=1; i<myTable.getRowCount(); ++i) {
+				sumVal+=(Integer) myTable.getValueAt(i,0);
+			}
+		}
+		return sumVal;
 	}
 }
