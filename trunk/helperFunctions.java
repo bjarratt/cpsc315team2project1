9f821fbe26836c38bd6ced8f9d43d6cbbe1ec471
DKATyler <DKATyler@c345011a-f2d2-11dd-b507-8b4ecd5210be>
2009-02-21 00:18:52 +0000

    Capitalization fix...

--- trunk/helperFunctions.java
+++ trunk/helperFunctions.java
@@ -1,52 +1,70 @@
+//Gabriel Copley
+//Revised 2/20/09
+//Converts variables into tables
 
-
-class helperFunctions {
+class HelperFunctions {
 	//Converts a given variable into a table
-	public static table convertToTable(table myTable, String variable) {
+	public static Table convertToTable(Table myTable, String variable) {
 		for(int ctr=0; ctr<myTable.getColumnCount(); ++ctr) {
 			System.out.println("MYTABLE: " + myTable.getValueAt(ctr, 0));
 		}
 		variable=variable.trim();
-		System.out.println("Parsing: " + variable);
-		compareOps drewOps=new compareOps();
-		table retTable;
+		System.out.println("helperFuncs Parsing: " + variable);
+		Table retTable;
 		int curCol=0;
-		for(curCol=0; curCol<myTable.getColumnCount() && myTable.getColumnName(curCol)!=variable; ++curCol);
-//		System.out.println(table.getColumnName(0));
-//		if(curCol!=table.getColumnCount()) {
-		if(variable.equals("A")) {
-		System.out.println("Parsing3: " + variable);
-			System.out.println("Converted to table");
-		    retTable=new table(myTable.getRowCount(), 1);
+		//Attempt lookup of column
+		for(curCol=0; curCol<myTable.colNames.size() && !myTable.colNames.get(curCol).equals(variable); ++curCol);
+		if(curCol!=myTable.colNames.size()) {
+			System.out.println("helperFuncs Parsing3: " + variable);
+			System.out.println("helperFuncs Converted to table");
+		    retTable=new Table(myTable.getRowCount(), 1);
 			for(int curRow=0; curRow<myTable.getRowCount(); ++curRow)
 				retTable.setValueAt(myTable.getValueAt(curRow,0), curRow, 0);
-
-			for(int ctr=0; ctr<myTable.getRowCount(); ++ctr) {
-				System.out.println("LArg: " + myTable.getValueAt(ctr, 0));
-			}
-			System.out.println("Converted to table success");
+			retTable.colTypes.add(myTable.colTypes.get(curCol));
+			System.out.println("helperFuncs Converted to table success");
 		}
 		else {
-			System.out.println("Parsing4: " + variable);
-			if(variable.contains(".")) {
+			try {
+			System.out.println("helperFuncs Parsing4: " + variable);
+			if(variable.contains("\"") || variable.contains("\'")) {
+				//Strip Quotes
+				if(variable.contains("\""))
+					variable=variable.substring(variable.indexOf("\"")+1, variable.lastIndexOf("\""));
+				else
+					variable=variable.substring(variable.indexOf("\'")+1, variable.lastIndexOf("\'"));
+				//Return as string
+				retTable=createTable(myTable.getRowCount(), variable);
+				retTable.colTypes.add(CompareOps.STRING);
+			}
+			else if(variable.contains(".")) {
 				System.out.println("Double: " + variable);
 				retTable=createTable(myTable.getRowCount(), Double.parseDouble(variable));
+				retTable.colTypes.add(CompareOps.DOUBLE);
 			}
-			else if(variable==String.valueOf(drewOps.TRUE))
-				retTable=createTable(myTable.getRowCount(), drewOps.TRUE);				
-			else if(variable==String.valueOf(drewOps.FALSE))
-				retTable=createTable(myTable.getRowCount(), drewOps.FALSE);				
-			else try {
+			else if(variable==String.valueOf(CompareOps.TRUE)) {
+				retTable=createTable(myTable.getRowCount(), CompareOps.TRUE);
+				retTable.colTypes.add("Boolean");
+			}
+			else if(variable==String.valueOf(CompareOps.FALSE)) {
+				retTable=createTable(myTable.getRowCount(), CompareOps.FALSE);
+				retTable.colTypes.add("Boolean");
+			}
+			else {
 				retTable=createTable(myTable.getRowCount(), Integer.parseInt(variable));
+				retTable.colTypes.add(CompareOps.INTEGER);
+			}
 			} catch(NumberFormatException e) {
+				System.err.println("ERROR, Unknown type?");
+				//Give up, hope a string works!
 				retTable=createTable(myTable.getRowCount(), variable);
+				retTable.colTypes.add("String");
 			}
 		}
 		return retTable;
 	}
 	//creates a table with initial value variable, cols=1, and the given number of rows
-	public static table createTable(int rows, Object variable) {
-	    table retTable=new table(rows, 1);
+	public static Table createTable(int rows, Object variable) {
+	    Table retTable=new Table(rows, 1);
 		for(int curRow=0; curRow<rows; ++curRow)
 			retTable.setValueAt(variable, curRow, 0);
 		return retTable;
