d4bc650fee01abdd430ac6cea46d48dd8c1d703e
DKATyler <DKATyler@c345011a-f2d2-11dd-b507-8b4ecd5210be>
2009-03-02 17:30:50 +0000

    Eh, got bored, added a restore db function to loadDb. Modified: TableOps to support "" as null loadDb added function TestMain to load the restoreDb.txt file if it exists Interface to save the db on exit

--- TestMain.java
+++ TestMain.java
@@ -1,15 +1,64 @@
-/* Created by Brandon Jarrat, Andrew Reagan, and Gabriel Copley
- * CPSC 315 Team Project 1
- * Airline Managing application with an SQL-like database implementation
- */
+import java.util.Vector;
+import static java.lang.System.*;
 
 class TestMain {
 	public static void main(String args[]) {
-
-		Interface window = new Interface();
+		/* Pass in nulls for now, may be useful to pass in an instantiated class
+		 * later on.
+		 * 
+		 * Be sure to use the Close button and not the windowing X or else...
+		 */
+		AddPassenger ap = new AddPassenger(null);
+		AddFlight af 	= new AddFlight(null);
+		AddTicket at	= new AddTicket(null);
+		GetInfo gi		= new GetInfo(null);
+		Boolean running = true;
+		
+		Interface window = new Interface(ap,af,at,gi);
 		window.setVisible(true);
+        if(!loadDb.load("restoreDb.txt") && !loadDb.load("SampleData.txt")) {
+        	//Both db load attempts failed, create an empty database
+   			System.err.println("Error Loading SampleData.txt, blank db created");
+        	TableOps.createTable("FlightInfo (flight# INTEGER, plane# INTEGER, passenger# INTEGER, startCity STRING, stopCity STRING, mealInclude STRING, departure STRING)");
+        	TableOps.createTable("PassengerInfo (name STRING, addr STRING, passenger# INTEGER, age INTEGER)");
+   			TableOps.createTable("TicketInfo (flight# INTEGER, passenger# INTEGER, class STRING, seat STRING, price DOUBLE)");
+        }
 
-        loadDb.load("SampleData.txt");
-
+//        Table tickets = TableOps.select("* FROM FlightInfo");
+//        UserTables.printEntireTable(tickets);
+    	Table displayTable=TableOps.select("mealInclude,flight#, plane#, startCity, stopCity, departure" +
+				   "FROM FlightInfo" +
+	 			  "WHERE mealInclude=TRUE");
+    	UserTables.printEntireTable(displayTable);
+		/*
+		 * window.setVisible(true);
+		 * 
+		 * Vector<String> colNames=new Vector<String>(); Vector<String>
+		 * colTypes=new Vector<String>(); Vector<Object> addRow=new
+		 * Vector<Object>(); colNames.add("A"); colNames.add("I");
+		 * colTypes.add(CompareOps.DOUBLE); colTypes.add(CompareOps.INTEGER);
+		 * Table testObj=new Table("B", colNames, colTypes); addRow.add(0.0); //
+		 * D addRow.add(10); // I testObj.addRow(addRow);
+		 * addRow.setElementAt(1.0, 0); // D addRow.setElementAt(20, 1); // I
+		 * testObj.addRow(addRow); addRow.setElementAt(2.0, 0);
+		 * addRow.setElementAt(30, 1); testObj.addRow(addRow);
+		 * addRow.setElementAt(3.0, 0); addRow.setElementAt(40, 1);
+		 * testObj.addRow(addRow); addRow.setElementAt(1.0, 0);
+		 * addRow.setElementAt(20, 1); testObj.addRow(addRow);
+		 * TableOps.db.add(testObj); TableOps.createTable("A (A "+
+		 * CompareOps.DOUBLE + ")"); TableOps.insertInto("A VALUES 1.0");
+		 * TableOps.insertInto("A VALUES 2.0");
+		 * TableOps.insertInto("A VALUES 3.0");
+		 * TableOps.insertInto("A VALUES 1.0"); // for(int ctr=0;
+		 * ctr<testObj.getRowCount(); ++ctr) { // System.out.println("TESTOBJ "
+		 * + testObj.getValueAt(ctr, 0) +'\t'+ testObj.getValueAt(ctr, 1)); // }
+		 * // Table retTable=WhereClass.where(testObj, "A > 1.1"); Table
+		 * retTable=TableOps.select("A,I FROM A,B WHERE I > 2");
+		 * out.println("\n\nThis is my final return table!\n");
+		 * UserTables.printEntireTable(retTable);
+		 * retTable=TableOps.select("A FROM A WHERE A > 2");
+		 * out.println("\n\nThis is my final return table!\n");
+		 * UserTables.printEntireTable(retTable);
+		 */
 	}
 }
--- loadDb.java
+++ loadDb.java
@@ -1,22 +1,69 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 class loadDb {
-	static void load(String fileName) {
+	//Loads the database
+	static boolean load(String fileName) {
 		try {
-			String nextLine;
-			String currentTable;
-			BufferedReader in;
-			in = new BufferedReader(new FileReader(fileName));
-	        while ((nextLine = in.readLine())!= null) {
-	        	currentTable=nextLine.substring(0,nextLine.indexOf(' '));
-	        	TableOps.createTable(nextLine);
-	        	while((nextLine = in.readLine())!= null && !nextLine.equals("")) {
-	        		TableOps.insertInto(currentTable + " VALUES (" + nextLine + ")");
-	        	}
-	        }
+			BufferedReader in= new BufferedReader(new FileReader(fileName));
+			try {
+				String nextLine;
+				String currentTable;
+				while ((nextLine = in.readLine())!= null) {
+					currentTable=nextLine.substring(0,nextLine.indexOf(' '));
+					TableOps.createTable(nextLine);
+					while((nextLine = in.readLine())!= null && !nextLine.equals("")) {
+						TableOps.insertInto(currentTable + " VALUES (" + nextLine + ")");
+					}
+				}
+			} finally {
+				in.close();
+			}
 		} catch(Exception e) {
-			System.err.println(e);
-		};
+			return false;
+		}
+		return true;
+	}
+	//Saves the database to the given file
+	static boolean saveDatabase(String fileName) {
+		try {
+			BufferedWriter out= new BufferedWriter(new FileWriter(fileName));
+			try {
+				for(int tableCtr=0; tableCtr<TableOps.db.size(); ++tableCtr) {
+					Table curTable=TableOps.db.get(tableCtr);
+					out.write(curTable.getName() + " (" );
+					if(curTable.getColumnCount()!=0)
+						out.write(curTable.getColName(0) + " " + curTable.getColType(0));
+					for(int colCtr=1; colCtr<curTable.getColumnCount(); ++colCtr) {
+						out.write(", " + curTable.getColName(colCtr) + " " + curTable.getColType(colCtr));
+					}
+					out.write(")");
+					out.newLine();
+					for(int rowCtr=0; rowCtr<curTable.getRowCount(); ++rowCtr) {
+						try {
+							out.write(curTable.getValueAt(rowCtr,0).toString());
+						} catch(NullPointerException e) {
+							out.write("");
+						}
+						for(int colCtr=1; colCtr<curTable.getColumnCount(); ++colCtr) {
+							try {
+								out.write("," + curTable.getValueAt(rowCtr,colCtr).toString());
+							} catch(NullPointerException e) {
+								out.write(",");
+							}
+						}
+						out.newLine();
+					}
+					out.newLine();
+				}
+			} finally {
+				out.close();
+			}
+		} catch(Exception e) {
+			return false;
+		}
+		return true;
 	}
 }
